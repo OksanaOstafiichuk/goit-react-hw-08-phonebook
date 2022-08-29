8143f7c8360538b9474b7eea7456532817af5cb5
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-08-29 19:40:55 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,45 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query'
+import {
+  persistStore,
+persistReducer,
+
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+import { authApi } from './authApi';
 import { contactsApi } from './contactsApi'
 import { phoneBookSlice } from './phoneBookSlice';
+import { userSlice } from './userSlice';
+
+const persistConfig = {
+    key: 'user',
+    storage,
+   whitelist: ['token'],
+}
 
 export const store = configureStore({
     reducer: {
     [contactsApi.reducerPath]: contactsApi.reducer,
-    phoneBook: phoneBookSlice.reducer
+    [authApi.reducerPath]: authApi.reducer,
+    phoneBook: phoneBookSlice.reducer,
+    user: persistReducer(persistConfig, userSlice.reducer),
   },
    middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware(),
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
     contactsApi.middleware,
+    authApi.middleware,
   ],
 })
 
 setupListeners(store.dispatch)
+export const persistor = persistStore(store)
