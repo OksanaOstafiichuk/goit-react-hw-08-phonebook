81feda39a8ba20f5c02b6c717a66e3c3956a9fa5
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-08-29 12:09:25 +0300

    Update App.styled.jsx

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
 
 export const PhoneBook = styled.div`
-  margin: 50px 0 0 30px;
+  margin: 50px auto;
+  padding: 0 20px;
   font-family: monospace;
   font-size: 24px;
-  width: 800px;
+  max-width: 1440px;
 `;
 
-export const Title = styled.h1`
-  font-size: 54px;
-`;
+// export const Title = styled.h1`
+//   font-size: 54px;
+// `;
 
 export const Contacts = styled.h2`
   font-size: 32px;
